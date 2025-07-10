b59b1905db6778a282af216ea7adcdeee2bcd716
Mehul-175 <mehul110359@gmail.com>
2025-07-10 15:38:04 +0530

    setup: initialized daisyUI, TanStack Query, and axios

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,31 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import Onboading from './pages/Onboading.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import Notifications from './pages/Notifications.jsx'
+import { Toaster } from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
+import { axiosInstance } from './lib/axios.js'
 
 const App = () => {
   return (
-    <div className='text-3xl text-green-700'>SmoothConnect</div>
+    <div className='h-screen' data-theme="luxury"> 
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/onboarding" element={<Onboading />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/call" element={<CallPage />} />
+          <Route path="/notifications" element={<Notifications />} />
+        </Routes>
+        <Toaster />
+    </div>
   )
 }
 
---
+++ Client/src/pages/Onboading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Onboading = () => {
+  return (
+    <div>Onboading</div>
+  )
+}
+
+export default Onboading
