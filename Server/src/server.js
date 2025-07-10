c5c166c5dc67d1542b1da184afa71679c152168e
Mehul-175 <mehul110359@gmail.com>
2025-07-10 15:41:55 +0530

    Add CORS in server setup

--- Server/src/server.js
+++ Server/src/server.js
@@ -3,6 +3,7 @@ import "./configEnv.js"
 // dotenv.config();
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import connectDB from "./lib/db.js";
@@ -13,6 +14,10 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
