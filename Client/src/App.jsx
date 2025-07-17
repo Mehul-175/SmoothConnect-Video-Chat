eba10eb9d22bc878bad4b096f37f1101d36f8c5d
Mehul-175 <mehul110359@gmail.com>
2025-07-17 14:25:31 +0530

    Add Onboarding Page with API integration

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,41 +1,67 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router'
-import HomePage from './pages/HomePage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import SignUpPage from './pages/SignUpPage.jsx'
-import Onboading from './pages/Onboading.jsx'
-import ChatPage from './pages/ChatPage.jsx'
-import CallPage from './pages/CallPage.jsx'
-import Notifications from './pages/Notifications.jsx'
-import { Toaster } from 'react-hot-toast'
-import { useQuery } from '@tanstack/react-query'
-import { axiosInstance } from './lib/axios.js'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import Onboading from "./pages/Onboading.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import Notifications from "./pages/Notifications.jsx";
+import { Toaster } from "react-hot-toast";
+import PageLoader from "./components/PageLoader.jsx";
+import useAuthUser from "./hooks/useAuthUser.js";
 
 const App = () => {
-  const { data: authData, isLoading, error } = useQuery({
-    queryKey: ["authUser"],
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me")
-      return res.data
-    },
-    retry: false
-  })
-  const authUser = authData?.user
-  return (
-    <div className='h-screen' data-theme="synthwave"> 
-        <Routes>
-          <Route path="/" element={authUser ? <HomePage /> : <Navigate to = "/login" />} />
-          <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to = "/" />} />
-          <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to = "/" />  } />
-          <Route path="/onboarding" element={authUser ? <Onboading /> : <Navigate to = "/login" />} />
-          <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to = "/login" />} />
-          <Route path="/call" element={authUser ? <CallPage /> : <Navigate to = "/login" />} />
-          <Route path="/notifications" element={authUser ? <Notifications /> : <Navigate to = "/login" />} />
-        </Routes>
-        <Toaster />
-    </div>
-  )
-}
+  const { isLoading, authUser } = useAuthUser();
 
-export default App
+  const isAuthenticated = Boolean(authUser);
+
+  const isOnboarded = authUser?.IsOnboarded;
+
+  if (isLoading) return <PageLoader />;
+  return (
+    <div className="h-screen" data-theme="synthwave">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <HomePage />
+            ) : (
+              <Navigate to={isAuthenticated ? "/onboarding" : "/login"} />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/onboarding"
+          element={isAuthenticated ? <Onboading /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/chat"
+          element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/call"
+          element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/notifications"
+          element={
+            isAuthenticated ? <Notifications /> : <Navigate to="/login" />
+          }
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
---
+++ Client/src/components/PageLoader.jsx
@@ -0,0 +1,9 @@
+const PageLoader = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+         <div className="w-10 h-10 border-4 border-dashed rounded-full animate-spin border-primary"></div>
+    </div>
+  )
+}
+
+export default PageLoader
